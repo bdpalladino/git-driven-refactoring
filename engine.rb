d00e14541e53196a2218b983b6f34151866142a5
Brian Palladino <brian.palladino@excella.com>
2018-01-21 19:33:27 -0500

    Allow user to set remote start delay

--- engine.rb
+++ engine.rb
@@ -1,23 +1,23 @@
 class engine
   REMOTE_START_DEVICES = [
-    "iphone",
-    "android",
-    "fob"
+    'iphone',
+    'android',
+    'fob'
   ]
 
   def start
-    puts "Start Engine"
+    puts 'Start Engine'
   end
 
-  def delay_remote_start
-    puts "Delay remote start"
+  def delay_remote_start(seconds=0)
+    puts "Delay remote start by #{seconds} seconds" 
   end
 
   def hill_assist
-    puts "Increase power if going up hill"
+    puts 'Increase power if going up hill'
   end
 
   def engine_power
-    puts "50 watts!"
+    puts '50 watts!'
   end
 end
