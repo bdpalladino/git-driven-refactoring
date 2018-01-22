55cf1997c488ab876f8f23cb3c69018c0ea6edb0
Brian Palladino <brian.palladino@excella.com>
2018-01-21 20:23:46 -0500

    Modifying the class instead of extending it - very naughty

--- check_engine.rb
+++ check_engine.rb
@@ -1,18 +1,22 @@
 class check_engine
-  def no_shorts_detected
-    spark_plugs.all? do |spark_plug|
-      spark_plug.connected?
+  def spark_plugs_connected?
+    all_connected = spark_plugs.all? { |spark_plug| spark_plug.connected? }
+
+    if all_connected
+      true
+    else
+      raise 'Electrical short detected'
     end
   end
 
   def notify_all_clear
-    'No engine shorts detected'
+    'No engine issues detected'
   end
 
-  def detect_engine_short
-    unless no_shorts_detected
-      raise 'Electrical short detected'
-    else
+  def detect_issues
+    if spark_plugs_connected
       notify_all_clear
+    else
+      raise 'Something went wrong'
   end
 end
